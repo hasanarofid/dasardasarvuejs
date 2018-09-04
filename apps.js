011cd123a765b0f42249c1c357311fa270cd0632
hasanarofid <hasanarofid@gmail.com>
2018-09-04 13:42:43 +0700

    selesai

--- apps.js
+++ apps.js
@@ -1,6 +1,38 @@
 new Vue({
-  el: "app",
+  el: "#app",
   data: {
-    pesan: "Ini cara menggunakan v-text"
+    pesan: "<b>Ini Vue Js</b><i> Hasanarofid.com</i>"
+  }
+});
+
+//mutidata
+
+new Vue({
+  el: "#app2",
+  data: {
+    pesan1: "Vue Js pertama Hasanarofid.com",
+    pesan2: "Vue Js kedua Hasanarofid.com",
+    pesan3: "Vue Js ketiga Hasanarofid.com"
+  }
+});
+
+new Vue({
+  el: "#app3",
+  data: {
+    pesanapp3: "pesan kombinasi pertama",
+    pesanapp4: "pesan kombinasi kedua",
+    pesanapp5: "<b>pesan kombinasi ketiga</b>"
+  }
+});
+
+new Vue({
+  el: "#app4",
+  data: {
+    text: "Hallo, Hasanarofid.com",
+    angka1: 100,
+    angka2: 10.3,
+    pilihan1: true,
+    pilihan2: false,
+    array: ["satu", "dua", "tiga"]
   }
 });
